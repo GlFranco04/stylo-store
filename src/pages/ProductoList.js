befddbbf913cb715b668544017bdc26de6d0c8bb
Gabriel Andres Lopez Franco <gabrielandreslopezfranco04@gmail.com>
2024-09-18 16:55:56 -0400

    Primer commit del proyecto Stylo Store

---
+++ src/pages/ProductoList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import ProductoService from '../services/ProductoService';
+
+function ProductoList() {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    // Llamada a la API para obtener los productos
+    ProductoService.obtenerProductos()
+      .then(response => {
+        console.log('Productos recibidos:', response.data);  // Para depuración
+        setProductos(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los productos:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Productos</h1>
+      <ul>
+        {productos.length > 0 ? (
+          productos.map(producto => (
+            <li key={producto.id}>           
+              {producto.id} - {producto.nombre} - {producto.descripcion} - ${producto.precio}
+            </li>
+          ))
+        ) : (
+          <p>No hay productos disponibles.</p>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductoList;
